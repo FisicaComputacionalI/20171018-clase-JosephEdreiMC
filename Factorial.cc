bc79d780edfa9cdfd098e204f8e46cce45a58da7
JosephEdreiMC <joseph.edrei.mc@gmail.com>
2017-10-19 00:41:29 -0500

    Create Factorial.cc
    Programa que nos da el factorial de un número entero positivo.

---
+++ Factorial.cc
@@ -0,0 +1,24 @@
+// Programa que pide ingreso de variables por el usuario y nos da el factorial de un número
+// Fecha:18 Octubre 2017
+
+#include <iostream>
+using namespace std;
+int main ()
+{
+
+int N=0;
+cout <<"Dame un numero entero positivo" << endl;
+cin >> N;
+
+int M=1;
+int F=1;
+
+ while (M!=N){
+  M=M+1;
+  F=F*M;
+}
+
+cout<< "Valor final " << F<< endl;
+return 0;
+
+}
